cff550b6c837a55934d53fd0a2145afd69dd1866
Claudia Moreno Martinez <cmormar9@upv.edu.es>
2025-10-17 23:36:00 +0200

    Añadida implementación modificadas de la clase ListArray

--- list/ListArray.h
+++ list/ListArray.h
@@ -1,4 +1,8 @@
-#include <ostream>
+#ifndef LISTARRAY_H
+#define LISTARRAY_H
+
+#include <stdexcept>
+#include <iostream>
 #include "List.h"
 
 template <typename T>
@@ -9,19 +13,103 @@ private:
     int n;
     static const int MINSIZE = 2;
 
-    void resize(int new_size);
-
-public:
-    ListArray();
-    ~ListArray();
-
-    T operator[](int pos);
-    friend std::ostream& operator<<(std::ostream &out, const ListArray<T> &list) {
-        for (int i = 0; i < list.n; ++i) {
-            out << list.arr[i] << " ";
-        }
-        return out;
+    void resize(int new_size){
+    T* new_arr = new T[new_size];      
+    for (int i = 0; i < n; ++i) {       
+        new_arr[i] = arr[i];
+    }
+    delete[] arr;                       
+    arr = new_arr;                      
+    max = new_size;                     
+	    
     }
 
-};
+public:
+int size() override {
+    return n;  
+}
 
+bool empty() override {
+    return n == 0;  
+}
+
+T get(int pos) override {
+    if (pos < 0 || pos >= n) {
+        throw std::out_of_range("Posición inválida!");
+    }
+    return arr[pos];  
+}
+
+void insert(int pos, T e) override {
+    if (pos < 0 || pos > n) {
+        throw std::out_of_range("Posición inválida!");
+    }
+    if (n == max) {
+        resize(max * 2);  
+    }
+    for (int i = n; i > pos; --i) {
+        arr[i] = arr[i - 1];  
+    }
+    arr[pos] = e;
+    ++n;
+}
+
+void append(T e) override {
+    insert(n, e);  
+}
+
+void prepend(T e) override {
+    insert(0, e); 
+}
+
+int search(T e) override {
+    for (int i = 0; i < n; ++i) {
+        if (arr[i] == e) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+
+T remove(int pos) override {
+    if (pos < 0 || pos >= n) {
+        throw std::out_of_range("Posición inválida!");
+    }
+    T removed = arr[pos];
+    for (int i = pos; i < n - 1; ++i) {
+        arr[i] = arr[i + 1];  
+    }
+    --n;
+    if (n < max / 4 && max / 2 >= MINSIZE) {
+        resize(max / 2);
+    }
+    return removed;
+}
+
+    ListArray(){
+	arr = new T[MINSIZE];
+	max = MINSIZE;
+	n=0;
+    }
+    ~ListArray(){
+	delete[] arr;	    
+    }
+
+  T operator[](int pos){
+	return get(pos);
+  }
+template <typename U>
+friend std::ostream& operator<<(std::ostream& out, const ListArray<U>& list);
+};
+template <typename U>
+std::ostream& operator<<(std::ostream& out, const ListArray<U>& list) {
+    out << "List => [\n";
+    for (int i = 0; i < list.n; ++i) {
+        out << "  " << list.arr[i] << "\n";
+    }
+    out << "]";
+    return out;
+}
+
+#endif
