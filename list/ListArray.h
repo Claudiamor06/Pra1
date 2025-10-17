0b299c63eb1ad891b5465cf79af72fabb7a57dfe
Claudia Moreno Martinez <cmormar9@upv.edu.es>
2025-10-17 23:21:32 +0200

    Movido ListArray.h a la carpeta list

--- list/ListArray.h
+++ list/ListArray.h
@@ -1,11 +1,27 @@
-#ifndef LISTARRAY_H
-#define LISTARRAY_H
-
+#include <ostream>
 #include "List.h"
-#include "iostream"
-#include "stdexcept"
+
 template <typename T>
- class ListArray: public List<T>{
+class ListArray : public List<T> {
+private:
+    T* arr;
+    int max;
+    int n;
+    static const int MINSIZE = 2;
 
+    void resize(int new_size);
 
- }
+public:
+    ListArray();
+    ~ListArray();
+
+    T operator[](int pos);
+    friend std::ostream& operator<<(std::ostream &out, const ListArray<T> &list) {
+        for (int i = 0; i < list.n; ++i) {
+            out << list.arr[i] << " ";
+        }
+        return out;
+    }
+
+};
+
